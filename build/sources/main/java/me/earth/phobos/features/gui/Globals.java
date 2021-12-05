69da45e6cef65ebe2daeef56d4fb2e712e634dd2
BerryPGz <55507273+BerryPGz@users.noreply.github.com>
2021-12-06 05:36:18 +0700

    Change

--- build/sources/main/java/me/earth/phobos/features/gui/Globals.java
+++
@@ -1,9 +0,0 @@
-package me.earth.phobos.features.gui;
-
-import net.minecraft.client.Minecraft;
-import net.minecraftforge.fml.client.FMLClientHandler;
-
-public class Globals {
-    public static final Minecraft mc = Minecraft.getMinecraft();
-    public static final FMLClientHandler clientHandler = FMLClientHandler.instance();
-}
--- build/sources/main/java/me/earth/phobos/util/Util.java
+++
@@ -1,9 +0,0 @@
-package me.earth.phobos.util;
-
-import net.minecraft.client.Minecraft;
-
-public
-interface Util {
-    Minecraft mc = Minecraft.getMinecraft ( );
-}
-
